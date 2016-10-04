176472c383d82b14a103d391b7f8eb889020f5a7
kcmartin <kcmartin@users.noreply.github.com>
2016-10-04 09:58:53 -0700

    Add Header component
    fixed typo in react-router imports

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from './header';
+
 export default class App extends Component {
   render() {
     return (
-      <div>Starting point</div>
+      <div>
+        <Header />
+        Starting point
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, Rout, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
 import reducers from './reducers';
