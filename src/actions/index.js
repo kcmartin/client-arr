222aa28802f30f613c43999d2598250fa180ad24
kcmartin <kcmartin@users.noreply.github.com>
2016-10-11 16:09:09 -0700

    Add programmatic navigation
    using browserHistory from react router

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -6,14 +7,19 @@ export function signinUser ({ email, password }) {
   // redux thunk allows action creator to return a function
   return function(dispatch) {
     // submit em/pw to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-    
-    // if req good:
-    // - update state to show user is authenticated
-    // - save JWT token
-    // - redirect to the route '/feature'
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      // javascript promises
+      .then(response => {
+        // if req good:
+        // - update state to show user is authenticated
+        // - save JWT token
 
-    // if req bad:
-    // - show error to user
+        // - redirect to the route '/feature' - programmatic navigation
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // if req bad:
+        // - show error to user now
+      });
   }
 }
