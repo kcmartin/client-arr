f81b019acd5ea1a39cd4df3b240d7e742013e2c6
kcmartin <kcmartin@users.noreply.github.com>
2016-10-31 11:50:38 -0700

    Add header logic for authentication state
    Show links depending on signed in or signed out

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+  // helper method
+  renderLinks() {
+    if (this.props.authenticated) {
+      // show a link to sign-out
+      return <li className="nav-item">
+        <Link className="nav-link" to="/signout">Sign Out</Link>
+      </li>
+    } else {
+      // show link to sign in our sign up
+      // returns an array of components
+      return [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/signin">Sign in</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">Sign Up</Link>
+        </li>
+      ];
+    }
+  }
+
   render () {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">ReduxAuth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign in
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+// flag to show if user is authed
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated
+  };
+}
+
+export default connect()(Header);
